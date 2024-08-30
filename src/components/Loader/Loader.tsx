19c4d3087ff59b74b7b16937fffd963c776fb724
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-08-30 14:40:05 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,12 +4,7 @@ import css from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <InfinitySpin
-        visible={true}
-        width="200"
-        color="#8276ff"
-        ariaLabel="infinity-spin-loading"
-      />
+      <InfinitySpin width="200" color="#8276ff" />
     </div>
   );
 }
