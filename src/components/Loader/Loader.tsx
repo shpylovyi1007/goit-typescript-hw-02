71bbd347f7c1d832c6cf1736b3f594eb0c0bb605
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-08-30 15:15:21 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className={css.loader}>
+    <div className={css.loader} aria-label="Завантаження">
       <InfinitySpin width="200" color="#8276ff" />
     </div>
   );
